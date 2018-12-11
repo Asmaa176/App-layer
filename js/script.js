3345b4687b1d8f66e5c7de08554782f243e86361
Asmaa176 <43960688+Asmaa176@users.noreply.github.com>
2018-12-11 18:34:20 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,27 @@
+﻿
+$(document).ready(function(){
+
+    // $('.page').click(function(){
+    //     $('#inner-list').slideToggle();
+    // });
+
+    $('.menu').click(function(){
+        $('#list').slideToggle();
+    });
+
+     $(window).resize(function(){
+        if($(window).width() > 767) {
+            $('#list').toggleClass('responsive-nav').css('display','');
+        }
+    });  
+    
+    
+    // var myIcone = document.getElementById('first');
+    // myIcone.onmouseover = function(){
+    // myIcone.classList.add("first-rotate");
+    // }
+    // myIcone.onmouseout = function(){
+    // myIcone.classList.add("first-rotate-back");
+    // }
+    
+});
